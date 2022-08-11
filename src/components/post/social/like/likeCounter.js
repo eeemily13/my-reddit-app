b0dbb2147577c41367ef7a56902a68c5524e907f
Emily Echarri <emily.echarri@gmail.com>
2022-08-11 13:17:39 -0700

    all files this push

--- src/components/post/social/like/likeCounter.js
+++ src/components/post/social/like/likeCounter.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 export default  function LikeCounter() {
-    const [count, setCount] = useState();
+    const [count, setCount] = useState(0);
     const increment = () => setCount(prevCount => prevCount + 1);
     const decrement = () => setCount(prevCount => prevCount -1 );
         
